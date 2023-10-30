51d219e64e2798b6072942a823631aff3babe007
DMITRII <fogonrog@gmail.com>
2023-10-30 11:53:49 +0300

    [engine base] added some scenes, secon d attemps

--- app/src/main/java/com/road/polytechnic/model/Event.java
+++ app/src/main/java/com/road/polytechnic/model/Event.java
@@ -1,5 +1,46 @@
 package com.road.polytechnic.model;
 public class Event {
-    private String[] persоns;
-    private String[] background;
+    private int name;
+    private int phrase;
+    private int person;
+    private int background;
+
+    public Event(int name, int phrase, int person, int background) {
+        this.name = name;
+        this.phrase = phrase;
+        this.person = person;
+        this.background = background;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public void setPhrase(int phrase) {
+        this.phrase = phrase;
+    }
+
+    public void setPerson(int person) {
+        this.person = person;
+    }
+
+    public void setBackground(int background) {
+        this.background = background;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public int getPhrase() {
+        return phrase;
+    }
+
+    public int getPerson() {
+        return person;
+    }
+
+    public int getBackground() {
+        return background;
+    }
 }
--- app/src/main/java/com/road/polytechnic/view/GameActivity.java
+++ app/src/main/java/com/road/polytechnic/view/GameActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -12,8 +13,21 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.road.polytechnic.R;
+import com.road.polytechnic.model.Event;
+
+import java.util.ArrayList;
 
 public class GameActivity extends AppCompatActivity {
+
+    private ImageView bg;
+    private ImageView person1;
+    private TextView phrase;
+    private TextView name;
+    private Event currentEvent;
+    private int numCurrentEvent;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,26 +38,94 @@ public class GameActivity extends AppCompatActivity {
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         setContentView(R.layout.activity_game);
 
-//        width = 884;
-//        height = 1073;      классика
-//        topMargin = 148;
-//
-//        width = 1084;
-//        height = 1273;      близко
-//        topMargin = 148;
+        bg = findViewById(R.id.imageView);
+        phrase = findViewById(R.id.phrase);
+        name = findViewById(R.id.name);
+        person1 = findViewById(R.id.person1);
 
-//        String[] persons = new String["add bg 1-1", ""];
-        ImageView person1 = findViewById(R.id.person1);
-        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) person1.getLayoutParams();
-        params.width = 884;
-        params.height = 1073;
-        params.topMargin = 148;
-        person1.setLayoutParams(params);
-
-        ImageView bg = findViewById(R.id.imageView);
         bg.setOnClickListener(v -> {
-            bg.setImageResource(R.drawable.bg1_2);
+            change();
+        });
+        phrase.setOnClickListener(v -> {
+            change();
+        });
+        name.setOnClickListener(v -> {
+            change();
+        });
+        person1.setOnClickListener(v -> {
+            change();
         });
 
+            currentEvent = new Event(R.string.name_Lexa, R.string.Lphrase_1,
+                    R.drawable.none, R.drawable.black_bg);
+
+            bg.setImageResource(currentEvent.getBackground());
+            person1.setImageResource(currentEvent.getPerson());
+            name.setText(currentEvent.getName());
+            phrase.setText(currentEvent.getPhrase());
+
+            numCurrentEvent = 1;
+        }
+        public void change() {
+            switch (numCurrentEvent) {
+                case 6 :
+                    currentEvent.setName(R.string.name_mama);
+                    name.setText(currentEvent.getName());
+
+                    currentEvent.setPhrase(R.string.Mphrase_2);
+                    phrase.setText(currentEvent.getPhrase());
+
+                    currentEvent.setPerson(R.drawable.mama_shock);
+                    person1.setImageResource(currentEvent.getPerson());
+                    numCurrentEvent++;
+                    break;
+                case 5 :
+                    currentEvent.setPhrase(R.string.Lphrase_3);
+                    phrase.setText(currentEvent.getPhrase());
+
+                    currentEvent.setName(R.string.name_Lexa);
+                    name.setText(currentEvent.getName());
+                    numCurrentEvent++;
+                    break;
+            case 4 :
+                currentEvent.setPhrase(R.string.Mphrase_1);
+                phrase.setText(currentEvent.getPhrase());
+
+                currentEvent.setPerson(R.drawable.mama_happy);
+                person1.setImageResource(currentEvent.getPerson());
+
+                currentEvent.setName(R.string.name_mama);
+                name.setText(currentEvent.getName());
+
+                currentEvent.setBackground(R.drawable.kitchen);
+                bg.setImageResource(currentEvent.getBackground());
+
+                numCurrentEvent++;
+                break;
+            case 3 :
+                currentEvent.setPhrase(R.string.Lphrase_1);
+                phrase.setText(currentEvent.getPhrase());
+
+                currentEvent.setBackground(R.drawable.black_bg);
+                bg.setImageResource(currentEvent.getBackground());
+
+                currentEvent.setPerson(R.drawable.none);
+                person1.setImageResource(currentEvent.getPerson());
+
+                numCurrentEvent++;
+                break;
+            case 2 :
+                currentEvent.setBackground(R.drawable.bg1_2);
+                bg.setImageResource(currentEvent.getBackground());
+                currentEvent.setPhrase(R.string.Lphrase_2);
+                phrase.setText(currentEvent.getPhrase());
+                numCurrentEvent++;
+                break;
+            case 1:
+                currentEvent.setBackground(R.drawable.bg1_1);
+                bg.setImageResource(currentEvent.getBackground());
+                numCurrentEvent++;
+                break;
+        }
     }
 }
